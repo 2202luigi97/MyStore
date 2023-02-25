314a5f1d80a2adbfe9ba87425947c06f0a996ec5
luisj <luisjrcuadra10@gmail.com>
2023-02-25 09:24:50 -0600

    CRUD

--- BL/BL_Cliente.cs
+++ BL/BL_Cliente.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using EL;
 using DAL;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Data;
 
 namespace BL
 {
@@ -12,7 +14,19 @@ namespace BL
     {
         public static int InsertCliente(Cliente Entidad)
         {
-            return DAL_Cliente.InsertCliente(Entidad); 
+            return DAL_Cliente.InsertCliente(Entidad);
+        }
+        public static int UpdateCliente(Cliente Entidad)
+        {
+            return DAL_Cliente.UpdateCliente(Entidad);
+        }
+        public static int DeleteCliente(Cliente Entidad)
+        {
+            return DAL_Cliente.DeleteCliente(Entidad);
+        }
+        public static DataTable SelectCliente(int IdCliente)
+        {
+            return DAL_Cliente.SelectCliente(IdCliente);
         }
     }
 }
